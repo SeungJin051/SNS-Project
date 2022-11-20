13ef5c2aacfc922e2d6b4e449005f22a57081458
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2022-11-21 01:18:09 +0900

    3.0 add databaseURL

--- src/fbase.js
+++ src/fbase.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
@@ -14,3 +16,5 @@ const app = initializeApp(firebaseConfig);
 
 export const firebaseInstance = getAuth();
 export const authService = getAuth(app);
+
+export const dbService = getFirestore();
