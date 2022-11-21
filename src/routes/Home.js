9415ac1ff8db9eae4600c40bf0bbf6ba97fe6d1b
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2022-11-22 00:49:11 +0900

    4.0 add upload clear update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,16 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import { useState, useEffect } from "react";
-import { addDoc, getDocs, collection } from "firebase/firestore";
-import { onSnapshot } from "firebase/firestore";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import Mind from "components/Mind";
+import { ref, uploadString, getDownloadURL } from "@firebase/storage";
+import { v4 } from "uuid";
 
 const Home = ({ userObj }) => {
   console.log(userObj);
   // for form
   const [mind, setMind] = useState("");
   const [minds, setMinds] = useState([]);
+  const [file, setFile] = useState("");
 
   useEffect(() => {
     // 실시간 구현
@@ -23,12 +25,35 @@ const Home = ({ userObj }) => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    await addDoc(collection(dbService, "minds"), {
+    let fileUrl = "";
+
+    // reference
+    if (file !== "") {
+      //파일 경로 참조 만들기
+      const fileRef = ref(storageService, `${userObj.uid}/${v4()}`);
+      //storage 참조 경로로 파일 업로드 하기
+      const response = await uploadString(fileRef, file, "data_url");
+      // console.log(response);
+      //storage 참조 경로에 있는 파일의 URL을 다운로드해서 fileUrl 변수에 넣어서 업데이트
+      fileUrl = await getDownloadURL(response.ref);
+    }
+
+    // 게시글 오브젝트
+    const mindObj = {
       text: mind,
       createdAt: Date.now(),
       creatorID: userObj.uid,
-    });
+      email: userObj.email,
+      fileUrl,
+    };
+
+    // 작성하기 누르면 mindObj 형태로 새로운 document 생성하여 minds 콜렉션에 넣기
+    await addDoc(collection(dbService, "minds"), mindObj);
+
+    //state 비워서 form 비우기
     setMind("");
+    //파일 미리보기 img src 비워주기
+    setFile("");
   };
 
   // event 안에 있는 tartget 안에 있는 value를 가져옴
@@ -38,17 +63,46 @@ const Home = ({ userObj }) => {
     } = event;
     setMind(value);
   };
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    // get file
+    const theFile = files[0];
+    // make read
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setFile(result);
+    };
+    // read file
+    reader.readAsDataURL(theFile);
+  };
+  // clear file
+  const onClearFile = () => setFile("");
+
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input
+          required={true}
+          minLength={5}
           value={mind}
           onChange={onChange}
           type="text"
           placeholder="Type your mind"
-          laxLength={120}
+          maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Send" />
+        {file && (
+          <div>
+            <img src={file} width="50px" height="50px" />
+            <button onClick={onClearFile}>취소</button>
+          </div>
+        )}
       </form>
       <div>
         {minds.map((mind) => (
