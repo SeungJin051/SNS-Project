6939d2aba30b2344c67abb518c5dc42f5df7dee0
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2022-11-22 21:28:24 +0900

    1.0

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 
-const Navigation = () => (
+const Navigation = ({ userObj }) => (
   <nav>
     <ul>
       <li>
         <Link to="/">Home</Link>
       </li>
       <li>
-        <Link to="/profile">myProfile</Link>
+        <Link to="/profile">{userObj.displayName}님의 프로필</Link>
       </li>
     </ul>
   </nav>
