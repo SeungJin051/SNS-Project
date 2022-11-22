234f72bf80207fcd4ac7e7c7f698ec6889360c47
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2022-11-23 02:01:08 +0900

    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import firebase from "firebase/compat/app";
 import { BrowserRouter } from "react-router-dom";
-import "./styles.css";
 
 console.log(firebase);
 
