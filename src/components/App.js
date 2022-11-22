ced116a617ee90cf3dca8ed723bd89192647f3fc
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2022-11-22 21:28:04 +0900

    5.0 add updateProfile

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import AppRouter from "./Router";
 import React, { useState, useEffect } from "react";
 import { authService } from "fbase";
+import { updateProfile } from "@firebase/auth";
 
 function App() {
   const [init, setInit] = useState(false);
@@ -13,20 +14,46 @@ function App() {
       if (user) {
         setIsLoggedIn(true);
         setUserObj(user);
+        // setUserObj({
+        //   displayName: user.displayName,
+        //   uid: user.uid,
+        //   updateProfile: (args) => user.updateProfile(args),
+        // });
+        // 로컬 닉네임 설정 ** 고민 후 결정하자 ~~~~~~~~~~~~~~~~
+        if (user.displayName === null) {
+          const name = user.email.split("@")[0];
+          // const name = "Anonymous";
+          user.displayName = name;
+        }
       } else {
         setIsLoggedIn(false);
       }
       setInit(true);
     });
   }, []);
+
+  // *
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    // 복사본을 만들어 랜더링을 유도
+    setUserObj(Object.assign({}, user));
+  };
+
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+        <AppRouter
+          isLoggedIn={isLoggedIn}
+          userObj={userObj}
+          refreshUser={refreshUser}
+        />
       ) : (
         "Initializing..."
       )}
-      {/* <footer> {new Date().getFullYear()} </footer> */}
+      <footer>
+        {new Date().getFullYear()} {new Date().getMonth()}{" "}
+        {new Date().getDate()} {new Date().getHours()}
+      </footer>
     </>
   );
 }
