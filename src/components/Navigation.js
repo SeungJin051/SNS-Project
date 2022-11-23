1f79362209d34a5f23ee8b95927982fbf42b5501
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2022-11-23 17:04:03 +0900

    2.0

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,8 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPenToSquare,
-  faUserEdit,
-  faUser,
-} from "@fortawesome/free-solid-svg-icons";
+import { faPenToSquare, faUserEdit } from "@fortawesome/free-solid-svg-icons";
+import "styles/NavAnimation.css";
+
 const Navigation = ({ userObj }) => (
   <nav>
     <ul style={{ display: "flex", justifyContent: "center", marginTop: 50 }}>
@@ -12,7 +10,7 @@ const Navigation = ({ userObj }) => (
         <Link to="/" tyle={{ marginRight: 10 }}>
           <FontAwesomeIcon
             icon={faPenToSquare}
-            color={"#04AAFF"}
+            color={"#8f8fff"}
             size="2x"
             className="search"
           />
@@ -22,20 +20,20 @@ const Navigation = ({ userObj }) => (
         <Link
           to="/profile"
           style={{
-            marginLeft: 10,
+            marginLeft: 25,
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
-            fontSize: 14,
+            fontWeight: "bold",
           }}
         >
           <FontAwesomeIcon
             icon={faUserEdit}
-            color={"#04AAFF"}
+            color={"#8f8fff"}
             size="2x"
             className="search"
           />
-          {userObj.displayName}의 프로필
+          {userObj.displayName} 프로필
         </Link>
       </li>
     </ul>
